cbd2cf09328a36292aaf512787d12c768d9c4160
farazhaider88 <farazhaider88@yahoo.com>
2020-08-11 12:54:15 +0500

    initial

---
+++ WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Faraz Haider on 09/08/2020.
+//  Copyright © 2020 Faraz Haider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
